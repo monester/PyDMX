93376179a629e803a86cbfc3c0cb26fbb0163783
Alexander Charykov <monester@gmail.com>
2021-07-06 19:28:05 +0200

    Update PyDMX.py

--- PyDMX.py
+++ PyDMX.py
@@ -1,38 +1,24 @@
 import serial
 import time
-import numpy as np
 
 
 class PyDMX:
-    def __init__(self,COM='COM8',Cnumber=512,Brate=250000,Bsize=8,StopB=2,use_prev_data=False,preserve_data_name="preserved_data.txt"):
+    def __init__(self,COM='COM8',Cnumber=512,Brate=250000,Bsize=8,StopB=2,use_prev_data=False):
         #start serial
         self.channel_num = Cnumber
-        #self.ser = serial.Serial(COM,baudrate=Brate,bytesize=Bsize,stopbits=StopB)
-        self.data = np.zeros([self.channel_num+1],dtype='uint8')
-        self.data[0] = 0 # StartCode
+        self.data = [0] * (self.channel_num + 1)
+        self.ser = serial.Serial(COM,baudrate=Brate,bytesize=Bsize,stopbits=StopB)
         self.sleepms = 50.0
         self.breakus = 176.0
         self.MABus = 16.0
-        # save filename
-        self.preserve_data_name = preserve_data_name
-        self.use_prev_data = use_prev_data
-        # load preserved DMX data
-        if use_prev_data:
-            try:
-                self.load_data()
-            except:
-                print("Something is wrong. please check data format!")
         
-    def set_random_data(self):
-        self.data[1:self.channel_num+1]= np.random.rand(self.channel_num)*255
-
     def set_data(self,id,data):
-        self.data[id]=data
+        self.data[id] = data
 
-    def set_datalist(self,list_id,list_data):
+    def set_datalist(self, list_id, list_data):
         try:
-            for id,data in zip(list_id,list_data):
-                self.set_data(id,data)
+            for id,data in zip(list_id, list_data):
+                self.set_data(id, data)
         except:
             print('list of id and data must be the same size!')
 
@@ -52,22 +38,13 @@ class PyDMX:
         time.sleep(self.sleepms/1000.0) # between 0 - 1 sec
 
     def sendzero(self):
-        self.data = np.zeros([self.channel_num+1],dtype='uint8')
+        self.data = [0] * (self.channel_num + 1)
         self.send()
-
-    def load_data(self):
-        self.data = np.loadtxt(self.preserve_data_name,dtype='int')        
-
-    def preserve_data(self):
-        np.savetxt(self.preserve_data_name,self.data)        
 
     def __del__(self):
         print('Close serial server!')
         # close with preserving current DMX data, I guess you may not need to reset DMX signal in this option.
-        if self.use_prev_data:
-            self.preserve_data()
-        else:
-            self.sendzero()
+        self.sendzero()
         self.ser.close()
     
 
